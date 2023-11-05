ba2832ac2d76d51302267264b3c772ec2fe5df7a
samuelalfred3 <samuelalfrednjuguna@email.com>
2023-11-05 13:37:33 +0300

    My prompt file

---
+++ myPrompt.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * _myPrompt - checks whether the standard input is connected to a terminal 
+ */
+
+void _myPrompt(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "myPrompt$ ", 10);
+}
+
+/**
+ * end_of_file - this handles the End of File
+ * @len: returns the value of getline function
+ * @buff: is the buffer
+ */
+void end_of_file(int len, char *buff)
+{
+	(void)buff;
+	if (len == -1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			free(buff);
+		}
+		exit(0);
+	}
+}
