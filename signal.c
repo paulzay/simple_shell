90c13b745a4746b4c21eb026da04a2a49e12c0f5
samuelalfred3 <samuelalfrednjuguna@email.com>
2023-11-06 11:22:57 +0300

    handles the signal

---
+++ signal.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * signal_handler - handles the SIGINT signal (Ctrl+C)
+ * @sig_num: the signal number to be handled
+ */
+void signal_handler(int sig_num)
+{
+	if (sig_num == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n#myPrompt$ ", 12);
+	}
+}
