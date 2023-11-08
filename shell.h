5c59641fbe44a1bc2731b6fde39d7fe395d1f369
samuelalfred3 <samuelalfrednjuguna@email.com>
2023-11-08 14:04:37 +0300

    Added additional functions

--- shell.h
+++ shell.h
@@ -10,9 +10,22 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <signal.h>
 #include <stddef.h>
+
+/**prompt_fn.c**/
 char *read_user_input();
+
 void printer(char *c);
+
+/**myPrompt.c**/
 void _myPrompt(void);
+void end_of_file(int len, char *buff);
+
+/**signal.c**/
+void signal_handler(int sig_num);
+
+/**shell_execution.c**/
+void exec(char **argv);
 
 #endif
