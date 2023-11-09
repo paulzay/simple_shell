c65334a990202fe2c394e961b06bda80f921b701
Paul Ogolla <paulotieno2@gmail.com>
2023-11-09 11:18:54 +0300

    update files to run shell

--- main.c
+++ main.c
@@ -1,40 +1,20 @@
 #include "shell.h"
 
-/**
- * main - is the entry point for execution.
- * Return: 0 on success
- */
-
-int main(void)
+int main(int argc, char *argv[])
 {
-	/*
-	char *user_input = prompt_and_input("Enter your name: ");
-	if (user_input != NULL)
-	{
-		write(STDOUT_FILENO, "hey ", 4);
-		write(STDOUT_FILENO, user_input, strlen(user_input));
-		free(user_input);
-	}
-	signal(SIGINT, signal_handler);
-	  while (1)
-	  {
-		  if (isatty(fileno(stdin)))
-		  {
-			  write(STDOUT_FILENO, "#myPrompt$ ", 11);
-		  }
-		  if (getline(&buff, &size, stdin) == -1)
-		  {
-			  perror("getline");
-			  break;
-		  }
-		  end_of_file(len, buff);
-		  free(buff);
-	  }
-	  */
-	while (true)
-	{
-		_myPrompt();
-		*read_user_input();
-	}
-	return (0);
+
+  while (1)
+  {     
+    _myPrompt();
+    
+    char *user_input = command();
+
+    char **tokens = tokenise(user_input, ' ');
+
+    exec(tokens);
+
+    free(tokens);
+    free(user_input);
+  }
+  return (0);
 }
--- shell.h
+++ shell.h
@@ -1,31 +1,62 @@
-#ifndef SHELL_H
-#define SHELL_H
+// #ifndef SHELL_H
+// #define SHELL_H
 
-#include <stdbool.h>
+// #include <stdbool.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <string.h>
+// #include <errno.h>
+// #include <sys/wait.h>
+// #include <sys/stat.h>
+// #include <sys/types.h>
+// #include <signal.h>
+// #include <stddef.h>
+
+// /**prompt_fn.c**/
+// char *read_user_input();
+
+// void printer(char *c);
+
+// /**myPrompt.c**/
+// void _myPrompt(void);
+// void end_of_file(int len, char *buff);
+
+// /**signal.c**/
+// void signal_handler(int sig_num);
+
+// /**shell_execution.c**/
+// void exec(char **argv);
+
+// #endif
+
+#ifndef _SHELL_H
+#define _SHELL_H
+
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <errno.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <signal.h>
-#include <stddef.h>
 
-/**prompt_fn.c**/
-char *read_user_input();
+// #include <stdbool.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <string.h>
+// #include <errno.h>
+// #include <sys/wait.h>
+// #include <sys/stat.h>
+// #include <sys/types.h>
+// #include <stddef.h>
 
-void printer(char *c);
 
-/**myPrompt.c**/
 void _myPrompt(void);
-void end_of_file(int len, char *buff);
-
-/**signal.c**/
-void signal_handler(int sig_num);
-
-/**shell_execution.c**/
+int _strlen(char *str);
+char **tokenise(char *str, char delimiter);
 void exec(char **argv);
+char *command();
 
 #endif
