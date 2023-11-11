fbccfd9b13c51bf2fb0e0a36d7422f9ecc81273a
samuelalfred3 <samuelalfrednjuguna@email.com>
2023-11-11 12:56:56 +0300

    Updated shell header file

--- shell.h
+++ shell.h
@@ -1,35 +1,3 @@
-// #ifndef SHELL_H
-// #define SHELL_H
-
-// #include <stdbool.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <string.h>
-// #include <errno.h>
-// #include <sys/wait.h>
-// #include <sys/stat.h>
-// #include <sys/types.h>
-// #include <signal.h>
-// #include <stddef.h>
-
- /*prompt_fn.c*/
-// char *read_user_input();
-
-// void printer(char *c);
-
- /**myPrompt.c**/
-// void _myPrompt(void);
-// void end_of_file(int len, char *buff);
-
- /**signal.c**/
-// void signal_handler(int sig_num);
-
- /**shell_execution.c**/
-// void exec(char **argv);
-
-// #endif
-
 #ifndef _SHELL_H
 #define _SHELL_H
 
@@ -40,7 +8,11 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <sys/wait.h>
-
+#include <errno.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <signal.h>
+#include <sys/stat.h>
 
 void _myPrompt(void);
 int _strlen(char *str);
