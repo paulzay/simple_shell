8abbc231c9c7ea3ba06ac8d70c0ce3030186ab1d
samuelalfred3 <samuelalfrednjuguna@email.com>
2023-11-11 12:30:06 +0300

    Updated header file

--- shell.h
+++ shell.h
@@ -13,19 +13,19 @@
 // #include <signal.h>
 // #include <stddef.h>
 
-// /**prompt_fn.c**/
+ /*prompt_fn.c*/
 // char *read_user_input();
 
 // void printer(char *c);
 
-// /**myPrompt.c**/
+ /**myPrompt.c**/
 // void _myPrompt(void);
 // void end_of_file(int len, char *buff);
 
-// /**signal.c**/
+ /**signal.c**/
 // void signal_handler(int sig_num);
 
-// /**shell_execution.c**/
+ /**shell_execution.c**/
 // void exec(char **argv);
 
 // #endif
@@ -41,17 +41,6 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 
-// #include <stdbool.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <string.h>
-// #include <errno.h>
-// #include <sys/wait.h>
-// #include <sys/stat.h>
-// #include <sys/types.h>
-// #include <stddef.h>
-
 
 void _myPrompt(void);
 int _strlen(char *str);
@@ -60,5 +49,6 @@ void exec(char **argv);
 char *command();
 int my_putchar(char c);
 void my_puts(char *str);
+void my_exit(char **arv);
 
 #endif
