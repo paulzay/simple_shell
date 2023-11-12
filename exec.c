ced8f5dd028e4381d857e570e4f2ad01561b5a49
Paul Ogolla <paulotieno2@gmail.com>
2023-11-12 22:21:43 +0300

    run ls

--- exec.c
+++ exec.c
@@ -17,22 +17,25 @@ void exec(char **argv)
   char *executable = _which(argv[0]);
   if (executable == NULL)
      printf("Executable not found: ls\n");
+   else
+   {
+      child_id = fork();
 
-	child_id = fork();
-	if (child_id == 0)
-	{
-		// char *args[] = { argv[0], NULL };
-		execve(executable, argv, envp);
-		perror(argv[0]);
-		exit(1);
-	}
-	else if (child_id > 0)
-	{
-		waitpid(child_id, NULL, 0);
-	}
-	else
-	{
-		perror("fork failed");
-		exit(1);
-	}
+      if (child_id == 0)
+      {
+        // char *args[] = { argv[0], NULL };
+        execve(executable, argv, envp);
+        perror(argv[0]);
+        exit(1);
+      }
+      else if (child_id > 0)
+      {
+        waitpid(child_id, NULL, 0);
+      }
+      else
+      {
+        perror("fork failed");
+        exit(1);
+      }
+  }
 }
