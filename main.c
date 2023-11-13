ed613741935a65fba9ab18152da2a5a8db052efa
samuelalfred3 <samuelalfrednjuguna@email.com>
2023-11-13 15:07:19 +0300

    modified the main file

--- main.c
+++ main.c
@@ -2,20 +2,30 @@
 
 int main()
 {
-  char *user_input;
-  char **tokens;
-  while (1)
-  {     
-    _myPrompt();
-    
-    user_input = command();
+	char *user_input;
+       	char **tokens;
+	while (1)
+	{
+		_myPrompt();
+		user_input = command();
 
-    tokens = tokenise(user_input, ' ');
-
-    exec(tokens);
-
-    free(tokens);
-    free(user_input);
-  }
-  return (0);
+		if (user_input != NULL)
+		{
+			tokens = tokenise(user_input, ' ');
+			if (tokens != NULL && tokens[0] != NULL)
+		{
+			if (strcmp(tokens[0], "exit") == 0)
+			{
+				my_exit(tokens);
+			}
+			else
+			{
+				exec(tokens);
+			}
+		}
+			free(tokens);
+			free(user_input);
+		}
+	}
+	return (0);
 }
--- signal.c
+++ signal.c
@@ -8,6 +8,7 @@ void signal_handler(int sig_num)
 {
 	if (sig_num == SIGINT)
 	{
-		write(STDOUT_FILENO, "\n#myPrompt$ ", 12);
+		write(STDOUT_FILENO, "\n", 1);
+		write(STDOUT_FILENO, "#myPrompt$ ", 12);
 	}
 }
