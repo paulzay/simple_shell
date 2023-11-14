e00008f8f9f478cda6af0a96ba856f6e6bdcb6b9
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2023-11-14 14:32:50 +0300

    Only Modified

--- _which.c
+++ _which.c
@@ -1,22 +1,24 @@
 #include "shell.h"
 
-char *_which(const char *command) {
-  char *path = _getenv("PATH");
-  char *delim = ":";
-  char *token = strtok(path, delim);
-  char *full_path = NULL;
+char *_which(const char *command)
+{
+	char *path = _getenv("PATH");
+	char *delim = ":";
+	char *token = strtok(path, delim);
+	char *full_path = (char *)malloc(strlen(command) + 1);
 
-  while (token != NULL) {
-    full_path = (char *)malloc(strlen(token) + strlen(command) + 2);
-    sprintf(full_path, "%s/%s", token, command);
-
-    if (access(full_path, F_OK) == 0) {
-      return full_path;
-    }
-
-    free(full_path);
-    token = strtok(NULL, delim);
-  }
-
-  return NULL;
+	if (full_path == NULL) {
+		perror("malloc failed");
+		exit(1);
+	}
+	while (token != NULL)
+	{
+		sprintf(full_path, "%s/%s", token, command);
+		if (access(full_path, F_OK) == 0) {
+			return full_path;
+		}
+		token = strtok(NULL, delim);
+	}
+	free(full_path);
+	return NULL;
 }
--- exec.c
+++ exec.c
@@ -8,41 +8,29 @@
 void exec(char **argv)
 {
 	int child_id;
-  char *executable ;
-  /*
-  char *path = _getenv(argv[0]);
+	char *executable;
 
-  char *envp[] = {path, NULL};
-  */
 	if (argv[0] && _strlen(argv[0]) < 2) {
 		return;
 	}
-
-  executable = _which(argv[0]);
-
-  if (executable == NULL)
-  {
-    printf("hsh %s: command not found\n", argv[0]);
-  }
-  if (executable != NULL)
-  {
-    child_id = fork();
-
-    if (child_id == 0)
-    {
-      /* char *args[] = { argv[0], NULL }; */
-      execve(executable, argv, NULL);
-      perror(argv[0]);
-      exit(1);
-    }
-    else if (child_id > 0)
-    {
-      waitpid(child_id, NULL, 0);
-    }
-    else
-    {
-      perror("fork failed");
-      exit(1);
-    }
-  }
+	executable = _which(argv[0]);
+	if (executable == NULL) {
+		printf("hsh %s: command not found\n", argv[0]);
+		return;
+	}
+	child_id = fork();
+	if (child_id == -1) {
+		perror("fork failed");
+		exit(1);
+	}
+	if (child_id == 0) {
+		execve(executable, argv, NULL);
+		perror(argv[0]);
+		exit(1);
+	} else {
+		if (waitpid(child_id, NULL, 0) == -1) {
+			perror("waitpid failed");
+			exit(1);
+		}
+	}
 }
