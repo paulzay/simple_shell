1221e60b43ac144bbfff924fab57f2d1201cee68
Paul Ogolla <paulotieno2@gmail.com>
2023-11-15 11:20:10 +0300

    fix betty

--- _strlen.c
+++ _strlen.c
@@ -1,14 +1,19 @@
 #include "shell.h"
 
+/**
+ * _strlen - function to get length of string
+ * @str: the string
+ * Return: length of string
+*/
 
 int _strlen(char *str)
 {
-  int count;
+	int count;
 
-  for (count = 0; *str != '\0'; str++)
-  {
-    count++;
-  }
+	for (count = 0; *str != '\0'; str++)
+	{
+		count++;
+	}
 
-  return (count);
+	return (count);
 }
--- _which.c
+++ _which.c
@@ -1,5 +1,10 @@
 #include "shell.h"
 
+/**
+ * _which - function to return executable path
+ * @command: the given command
+ * Return: NULL
+*/
 char *_which(const char *command)
 {
 	char *path = _getenv("PATH");
@@ -7,18 +12,18 @@ char *_which(const char *command)
 	char *token = strtok(path, delim);
 	char *full_path = (char *)malloc(strlen(command) + 1);
 
-	if (full_path == NULL) {
+	if (full_path == NULL)
+	{
 		perror("malloc failed");
 		exit(1);
 	}
 	while (token != NULL)
 	{
 		sprintf(full_path, "%s/%s", token, command);
-		if (access(full_path, F_OK) == 0) {
-			return full_path;
-		}
+		if (access(full_path, F_OK) == 0)
+			return (full_path);
 		token = strtok(NULL, delim);
 	}
 	free(full_path);
-	return NULL;
+	return (NULL);
 }
