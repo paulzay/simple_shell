f3c8650b3f8527227f56c38c5002b65d05be8e8a
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2023-11-16 13:37:40 +0300

    no modifications just verifying

--- main.c
+++ main.c
@@ -8,32 +8,26 @@
 int main(void)
 {
 	char *user_input;
+	char **tokens;
 	ssize_t chars = 0;
+	char *path;
 	size_t size = 0;
-
+	
 	signal(SIGINT, signal_handler);
 
 	while (chars != EOF)
 	{
 		_myPrompt();
 		chars = getline(&user_input, &size, stdin);
-		if (chars == -1)
-		{
-			perror("getline");
-			break;
-		}
-		if (chars == 1)
-		{
-			continue;
-		}
-		char **tokens = tokenise(user_input, ' ');
+		end_of_file(chars, user_input);
+		tokens = tokenise(user_input, ' ');
 		if (tokens != NULL && tokens[0] != NULL)
 		{
 			exec(tokens);
 		}
 		else
 		{
-			char *path = _which(tokens[0]);
+			path = _which(tokens[0]);
 			if (path)
 			{
 				exec(tokens);
@@ -44,9 +38,10 @@ int main(void)
 				tokens[0] = path;
 				exec(tokens);
 			}
+		
 		}
-		free(tokens);
 	}
+	free(tokens);
 	free(user_input);
 	return (0);
 }
